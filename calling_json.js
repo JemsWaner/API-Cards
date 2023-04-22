14565c805a577d3a07418dd18a33c2c2dfcb5bd9
JemsWaner <jemswaner@gmail.com>
2023-04-22 12:47:29 -0400

    Initial commit

---
+++ calling_json.js
@@ -0,0 +1,48 @@
+import { jsonText } from "./json.js";
+
+const edit = JSON.parse(jsonText);
+
+edit["browsers"]["chrome"] = {
+  name: "Firefox",
+};
+console.log(edit);
+
+//////////// Api rick and morty
+
+const INPUT = document.getElementById("numberCards");
+const BUTTON = document.getElementById("button");
+
+/////////// linking button and input number\\\\\
+BUTTON.addEventListener("click", async () => {
+  let randomNum = INPUT.value;
+  let random = Math.floor(Math.random() * randomNum);
+
+  const DIV = document.createElement("div");
+  const UL = document.createElement("ul");
+  UL.className = "cards-container";
+  document.body.appendChild(DIV);
+  DIV.appendChild(UL);
+
+  async function callingApi(url) {
+    try {
+      let request = await fetch(url);
+      let transform = await request.json();
+      let character = await transform.image;
+
+      return character;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  for (let i = 1; i <= random; i++) {
+    const LI = document.createElement("li");
+    const IMG = document.createElement("img");
+    IMG.src = await callingApi(
+      "https://rickandmortyapi.com/api/character/" + i
+    );
+    IMG.className = "images";
+    UL.appendChild(LI);
+    LI.appendChild(IMG);
+  }
+});
---
+++ json.js
@@ -0,0 +1,16 @@
+export const jsonText = `{
+    "browsers": {
+      "firefox": {
+        "name": "Firefox",
+        "pref_url": "about:config",
+        "releases": {
+          "1": {
+            "release_date": "2004-11-09",
+            "status": "retired",
+            "engine": "Gecko",
+            "engine_version": "1.7"
+          }
+        }
+      }
+    }
+  }`;
